bd159c1d90b69598df871bdcdafe8621f3548ffc
Benji63 <piat9871@gmail.com>
2023-06-07 13:23:25 +0200

    a

--- OpenVas/templates/task_creation.sh
+++ OpenVas/templates/task_creation.sh
@@ -3,8 +3,8 @@
 LOG_FILE="/opt/task_creation.log"
 
 echo "$(date): Script started" >> "$LOG_FILE"
-
-HOST_ID_FULl=$(omp -u admin -w admin -h 127.0.0.1 -p 9390 --xml '<create_target><name>local</name><hosts>127.0.0.1</hosts><alive_tests>ICMP, TCP-ACK Service &amp; ARP Ping</alive_tests></create_target>')
+XML="<create_target><name>local</name><hosts>$1</hosts><alive_tests>ICMP, TCP-ACK Service &amp; ARP Ping</alive_tests></create_target>"
+HOST_ID_FULl=$(omp -u admin -w admin -h $1 -p 9390 --xml "<create_target><name>local</name><hosts>$1</hosts><alive_tests>ICMP, TCP-ACK Service &amp; ARP Ping</alive_tests></create_target>")
 echo "$(date): HOST_ID_FULl: $HOST_ID_FULl" >> "$LOG_FILE"
 
 HOST_ID=$(echo "$HOST_ID_FULl" | grep -oP 'id="\K[^"]+')
@@ -12,10 +12,10 @@ echo "$(date): HOST_ID: $HOST_ID" >> "$LOG_FILE"
 
 #HOST_ID=c4f1bcd8-56e2-4c2b-ae62-2c38f30e5df4
 
-TASK_ID=$(omp -u admin -w admin -h 127.0.0.1 -p 9390 -C --target=$HOST_ID --config=daba56c8-73ec-11df-a475-002264764cea --name=scan)
+TASK_ID=$(omp -u admin -w admin -h $1 -p 9390 -C --target=$HOST_ID --config=daba56c8-73ec-11df-a475-002264764cea --name=scan)
 echo "$(date): TASK_ID: $TASK_ID" >> "$LOG_FILE"
 
-LAUNCHED_TASK=$(omp -u admin -w admin -h 127.0.0.1 -p 9390 -S $TASK_ID)
+LAUNCHED_TASK=$(omp -u admin -w admin -h $1 -p 9390 -S $TASK_ID)
 echo "$(date): LAUNCHED_TASK: $LAUNCHED_TASK" >> "$LOG_FILE"
 
 interval=60
@@ -38,8 +38,9 @@ echo "$report" >> "$LOG_FILE"
 
 echo "$report" | mail -s "Rapport de scan" openvasbachelor3@gmail.com
 
-echo "$(date): Exporting variables to /root/.profile" >> "$LOG_FILE"
-echo "export TARGET_ID=$HOST_ID" >> /root/.profile
-echo "export IP_TARGET=127.0.0.1" >> /root/.profile
+echo "$(date): Exporting variables to ~/.bashrc" >> "$LOG_FILE"
+echo "export TARGET_ID=$HOST_ID" >> ~/.bashrc
+echo "export IP_TARGET=$1" >> ~/.bashrc
+source ~/.bashrc
 
 echo "$(date): Script completed" >> "$LOG_FILE"
